aad950feebb9bf2feb4b409e3eab0ef8d963b284
Konstantin Markov <kmarkov@devexperts.com>
2026-02-16 22:46:44 +0000

    chore: update version to latest commit

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 ﻿// Version info - updated manually from docs/version.txt
 export const VERSION = {
     date: '2026-02-16',
-    commit: '2954545',
-    full: '2026-02-16 2954545'
+    commit: 'cfc2a67',
+    full: '2026-02-16 cfc2a67'
 };
