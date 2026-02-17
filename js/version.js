4a48a77046b1b64ab9d7fbd7fcc25dd727da2947
Konstantin Markov <kmarkov@devexperts.com>
2026-02-17 20:25:05 +0000

    Обновлена версия приложения до 991a9ff

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 ﻿// Version info - updated manually from docs/version.txt
 export const VERSION = {
     date: '2026-02-17',
-    commit: 'a6b9a8d',
-    full: '2026-02-17 a6b9a8d'
+    commit: '991a9ff',
+    full: '2026-02-17 991a9ff'
 };
