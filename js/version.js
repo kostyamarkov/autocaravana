4da7e0df7e4a640975b4cbd52ccd641adb7e3a06
Konstantin Markov <kmarkov@devexperts.com>
2026-02-17 00:22:32 +0000

    Update version 2026-02-16 b7c3e1f

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 ﻿// Version info - updated manually from docs/version.txt
 export const VERSION = {
     date: '2026-02-16',
-    commit: 'cfc2a67',
-    full: '2026-02-16 cfc2a67'
+    commit: 'b7c3e1f',
+    full: '2026-02-16 b7c3e1f'
 };
