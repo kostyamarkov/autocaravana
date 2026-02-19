ee64b00020aa59b170444d3a8737a032cb6d7f02
Konstantin Markov <kmarkov@devexperts.com>
2026-02-19 09:55:56 +0000

    chore: bump app version to 2026.02.19.0955

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 ﻿// Version info - updated manually from docs/version.txt
 export const VERSION = {
-    date: '2026.02.19.0939',
+    date: '2026.02.19.0955',
     commit: '',
-    full: '2026.02.19.0939'
+    full: '2026.02.19.0955'
 };
