777a808fc75e2a182a11263c67eab24ee91e53a8
Konstantin Markov <kmarkov@devexperts.com>
2026-03-02 21:46:56 +0000

    Update app version to current timestamp

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 ﻿// Version info - updated manually from docs/version.txt
 export const VERSION = {
-    date: '2026.03.02.1936',
+    date: '2026.03.02.2146',
     commit: '',
-    full: '2026.03.02.1936'
+    full: '2026.03.02.2146'
 };
